d5d748390261112d676c84e7f171daedf1758b81
Tejaswi Gowda <tejaswi@asu.edu>
2024-10-08 09:39:29 -0700

    ac

--- c3-icm/c3-icm.ino
+++ c3-icm/c3-icm.ino
@@ -6,86 +6,10 @@
 #include <WiFiClientSecure.h>
 #include <WebSocketsClient.h>
 #include <ESPmDNS.h>
-
 #include "ICM_20948.h" // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
 #define AD0_VAL 0
-
 ICM_20948_I2C myICM; // Otherwise create an ICM_20948_I2C object
 
-
-// Define a storage struct for the biases. Include a non-zero header and a simple checksum
-struct biasStore
-{
-  int32_t header = 0x42;
-  int32_t biasGyroX = 0;
-  int32_t biasGyroY = 0;
-  int32_t biasGyroZ = 0;
-  int32_t biasAccelX = 0;
-  int32_t biasAccelY = 0;
-  int32_t biasAccelZ = 0;
-  int32_t biasCPassX = 0;
-  int32_t biasCPassY = 0;
-  int32_t biasCPassZ = 0;
-  int32_t sum = 0;
-};
-
-void updateBiasStoreSum(biasStore *store) // Update the bias store checksum
-{
-  int32_t sum = store->header;
-  sum += store->biasGyroX;
-  sum += store->biasGyroY;
-  sum += store->biasGyroZ;
-  sum += store->biasAccelX;
-  sum += store->biasAccelY;
-  sum += store->biasAccelZ;
-  sum += store->biasCPassX;
-  sum += store->biasCPassY;
-  sum += store->biasCPassZ;
-  store->sum = sum;
-}
-
-bool isBiasStoreValid(biasStore *store) // Returns true if the header and checksum are valid
-{
-  int32_t sum = store->header;
-
-  if (sum != 0x42)
-    return false;
-
-  sum += store->biasGyroX;
-  sum += store->biasGyroY;
-  sum += store->biasGyroZ;
-  sum += store->biasAccelX;
-  sum += store->biasAccelY;
-  sum += store->biasAccelZ;
-  sum += store->biasCPassX;
-  sum += store->biasCPassY;
-  sum += store->biasCPassZ;
-
-  return (store->sum == sum);
-}
-
-void printBiases(biasStore *store)
-{
-  Serial.print(F("Gyro X: "));
-  Serial.print(store->biasGyroX);
-  Serial.print(F(" Gyro Y: "));
-  Serial.print(store->biasGyroY);
-  Serial.print(F(" Gyro Z: "));
-  Serial.println(store->biasGyroZ);
-  Serial.print(F("Accel X: "));
-  Serial.print(store->biasAccelX);
-  Serial.print(F(" Accel Y: "));
-  Serial.print(store->biasAccelY);
-  Serial.print(F(" Accel Z: "));
-  Serial.println(store->biasAccelZ);
-  Serial.print(F("CPass X: "));
-  Serial.print(store->biasCPassX);
-  Serial.print(F(" CPass Y: "));
-  Serial.print(store->biasCPassY);
-  Serial.print(F(" CPass Z: "));
-  Serial.println(store->biasCPassZ);
-
-}
 
 
 #include "esp_adc_cal.h"
@@ -270,13 +194,8 @@ void setup() {
   while (!initialized)
   {
 
-    // Initialize the ICM-20948
-    // If the DMP is enabled, .begin performs a minimal startup. We need to configure the sample mode etc. manually.
-#ifdef USE_SPI
-    myICM.begin(CS_PIN, SPI_PORT);
-#else
+
     myICM.begin(Wire, AD0_VAL);
-#endif
 
     Serial.print(F("Initialization of the sensor returned: "));
     Serial.println(myICM.statusString());
@@ -316,7 +235,7 @@ void setup() {
   //    INV_ICM20948_SENSOR_ORIENTATION                 (32-bit 9-axis quaternion + heading accuracy)
 
   // Enable the DMP orientation sensor
-  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_ORIENTATION) == ICM_20948_Stat_Ok);
+  success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_GAME_ROTATION_VECTOR) == ICM_20948_Stat_Ok);
 
   // Enable any additional sensors / features
   //success &= (myICM.enableDMPSensor(INV_ICM20948_SENSOR_RAW_GYROSCOPE) == ICM_20948_Stat_Ok);
@@ -328,7 +247,7 @@ void setup() {
   // Setting value can be calculated as follows:
   // Value = (DMP running rate / ODR ) - 1
   // E.g. For a 5Hz ODR rate when DMP is running at 55Hz, value = (55/5) - 1 = 10.
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Quat9, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Quat6, 0) == ICM_20948_Stat_Ok); // Set to the maximum
   //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Accel, 0) == ICM_20948_Stat_Ok); // Set to the maximum
   //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro, 0) == ICM_20948_Stat_Ok); // Set to the maximum
   //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro_Calibr, 0) == ICM_20948_Stat_Ok); // Set to the maximum
@@ -360,41 +279,6 @@ void setup() {
       ; // Do nothing more
   }
 
-  // Read existing biases from EEPROM
-  if (!EEPROM.begin(128)) // Allocate 128 Bytes for EEPROM storage. ESP32 needs this.
-  {
-    Serial.println(F("EEPROM.begin failed! You will not be able to save the biases..."));
-  }
-
-  biasStore store;
-
-  EEPROM.get(0, store); // Read existing EEPROM, starting at address 0
-  if (isBiasStoreValid(&store))
-  {
-    Serial.println(F("Bias data in EEPROM is valid. Restoring it..."));
-    success &= (myICM.setBiasGyroX(store.biasGyroX) == ICM_20948_Stat_Ok);
-    success &= (myICM.setBiasGyroY(store.biasGyroY) == ICM_20948_Stat_Ok);
-    success &= (myICM.setBiasGyroZ(store.biasGyroZ) == ICM_20948_Stat_Ok);
-    success &= (myICM.setBiasAccelX(store.biasAccelX) == ICM_20948_Stat_Ok);
-    success &= (myICM.setBiasAccelY(store.biasAccelY) == ICM_20948_Stat_Ok);
-    success &= (myICM.setBiasAccelZ(store.biasAccelZ) == ICM_20948_Stat_Ok);
-    success &= (myICM.setBiasCPassX(store.biasCPassX) == ICM_20948_Stat_Ok);
-    success &= (myICM.setBiasCPassY(store.biasCPassY) == ICM_20948_Stat_Ok);
-    success &= (myICM.setBiasCPassZ(store.biasCPassZ) == ICM_20948_Stat_Ok);
-
-    if (success)
-    {
-      Serial.println(F("Biases restored."));
-      printBiases(&store);
-    }
-    else
-      Serial.println(F("Bias restore failed!"));
-  }
-
-  Serial.println(F("The biases will be saved in two minutes."));
-  Serial.println(F("Before then:"));
-  Serial.println(F("* Rotate the sensor around all three axes"));
-  Serial.println(F("* Hold the sensor stationary in all six orientations for a few seconds"));
 
 
   Serial.println(F("IMU enabled"));
@@ -496,112 +380,76 @@ void TaskReadIMU(void *pvParameters) {
       prev_ms1 = millis();
     }
 
-
-  static unsigned long startTime = millis(); // Save the biases when the code has been running for two minutes
-  static bool biasesStored = false;
-  
-  icm_20948_DMP_data_t data;
+icm_20948_DMP_data_t data;
   myICM.readDMPdataFromFIFO(&data);
 
   if ((myICM.status == ICM_20948_Stat_Ok) || (myICM.status == ICM_20948_Stat_FIFOMoreDataAvail)) // Was valid data available?
   {
-    if ((data.header & DMP_header_bitmap_Quat9) > 0) // We have asked for orientation data so we should receive Quat9
+    //Serial.print(F("Received data! Header: 0x")); // Print the header in HEX so we can see what data is arriving in the FIFO
+    //if ( data.header < 0x1000) Serial.print( "0" ); // Pad the zeros
+    //if ( data.header < 0x100) Serial.print( "0" );
+    //if ( data.header < 0x10) Serial.print( "0" );
+    //Serial.println( data.header, HEX );
+
+    if ((data.header & DMP_header_bitmap_Quat6) > 0) // We have asked for GRV data so we should receive Quat6
     {
+      // Q0 value is computed from this equation: Q0^2 + Q1^2 + Q2^2 + Q3^2 = 1.
+      // In case of drift, the sum will not add to 1, therefore, quaternion data need to be corrected with right bias values.
+      // The quaternion data is scaled by 2^30.
+
+      //Serial.printf("Quat6 data is: Q1:%ld Q2:%ld Q3:%ld\r\n", data.Quat6.Data.Q1, data.Quat6.Data.Q2, data.Quat6.Data.Q3);
+
       // Scale to +/- 1
-      double q1 = ((double)data.Quat9.Data.Q1) / 1073741824.0; // Convert to double. Divide by 2^30
-      double q2 = ((double)data.Quat9.Data.Q2) / 1073741824.0; // Convert to double. Divide by 2^30
-      double q3 = ((double)data.Quat9.Data.Q3) / 1073741824.0; // Convert to double. Divide by 2^30
+      double q1 = ((double)data.Quat6.Data.Q1) / 1073741824.0; // Convert to double. Divide by 2^30
+      double q2 = ((double)data.Quat6.Data.Q2) / 1073741824.0; // Convert to double. Divide by 2^30
+      double q3 = ((double)data.Quat6.Data.Q3) / 1073741824.0; // Convert to double. Divide by 2^30
+
+
+      // Convert the quaternions to Euler angles (roll, pitch, yaw)
+      // https://en.wikipedia.org/w/index.php?title=Conversion_between_quaternions_and_Euler_angles&section=8#Source_code_2
 
       double q0 = sqrt(1.0 - ((q1 * q1) + (q2 * q2) + (q3 * q3)));
 
-      double qw = q0; // See issue #145 - thank you @Gord1
-      double qx = q2;
-      double qy = q1;
-      double qz = -q3;
+      double q2sqr = q2 * q2;
 
       // roll (x-axis rotation)
-      double t0 = +2.0 * (qw * qx + qy * qz);
-      double t1 = +1.0 - 2.0 * (qx * qx + qy * qy);
+      double t0 = +2.0 * (q0 * q1 + q2 * q3);
+      double t1 = +1.0 - 2.0 * (q1 * q1 + q2sqr);
       double roll = atan2(t0, t1) * 180.0 / PI;
 
       // pitch (y-axis rotation)
-      double t2 = +2.0 * (qw * qy - qx * qz);
+      double t2 = +2.0 * (q0 * q2 - q3 * q1);
       t2 = t2 > 1.0 ? 1.0 : t2;
       t2 = t2 < -1.0 ? -1.0 : t2;
       double pitch = asin(t2) * 180.0 / PI;
 
       // yaw (z-axis rotation)
-      double t3 = +2.0 * (qw * qz + qx * qy);
-      double t4 = +1.0 - 2.0 * (qy * qy + qz * qz);
+      double t3 = +2.0 * (q0 * q3 + q1 * q2);
+      double t4 = +1.0 - 2.0 * (q2sqr + q3 * q3);
       double yaw = atan2(t3, t4) * 180.0 / PI;
 
 
-      Serial.print(qw, 3);
+      Serial.print(q0, 3);
       Serial.print(" ");
-      Serial.print(qx, 3);
+      Serial.print(q1, 3);
       Serial.print(" ");
-      Serial.print(qy, 3);
+      Serial.print(q2, 3);
       Serial.print(" ");
-      Serial.print(qz, 3);
+      Serial.print(q3, 3);
       Serial.println();
-      quat.w = qw;
-      quat.x = qx;
-      quat.y = qy;
-      quat.z = qz;
+
+      quat.w = q0;
+      quat.x = q1;
+      quat.y = q2;
+      quat.z = q3;
     }
+
   }
 
   if (myICM.status != ICM_20948_Stat_FIFOMoreDataAvail) // If more data is available then we should read it right away - and not delay
   {
-    if (true/*!biasesStored*/) // Should we store the biases?
-    {
-      if (millis() > (startTime + 1*60*1000)) // Is it time to store the biases?
-      {
-        startTime = millis();
-        Serial.println(F("\r\n\r\n\r\nSaving bias data..."));
-
-        biasStore store;
-          
-        bool success = (myICM.getBiasGyroX(&store.biasGyroX) == ICM_20948_Stat_Ok);
-        success &= (myICM.getBiasGyroY(&store.biasGyroY) == ICM_20948_Stat_Ok);
-        success &= (myICM.getBiasGyroZ(&store.biasGyroZ) == ICM_20948_Stat_Ok);
-        success &= (myICM.getBiasAccelX(&store.biasAccelX) == ICM_20948_Stat_Ok);
-        success &= (myICM.getBiasAccelY(&store.biasAccelY) == ICM_20948_Stat_Ok);
-        success &= (myICM.getBiasAccelZ(&store.biasAccelZ) == ICM_20948_Stat_Ok);
-        success &= (myICM.getBiasCPassX(&store.biasCPassX) == ICM_20948_Stat_Ok);
-        success &= (myICM.getBiasCPassY(&store.biasCPassY) == ICM_20948_Stat_Ok);
-        success &= (myICM.getBiasCPassZ(&store.biasCPassZ) == ICM_20948_Stat_Ok);
-
-        updateBiasStoreSum(&store);
-      
-        if (success)
-        {
-          biasesStored = true; // Only attempt this once
-        
-          EEPROM.put(0, store); // Write biases to EEPROM, starting at address 0
-          EEPROM.commit(); // ESP32/SAMD/STM32 needs this
-  
-          EEPROM.get(0, store); // Read existing EEPROM, starting at address 0
-          if (isBiasStoreValid(&store))
-          {
-            Serial.println(F("Biases stored."));
-            printBiases(&store);
-            Serial.println(F("\r\n\r\n\r\n"));
-          }
-          else
-            Serial.println(F("Bias store failed!\r\n\r\n\r\n"));
-        }
-        else
-        {
-          Serial.println(F("Bias read failed!\r\n\r\n\r\n"));
-        }
-      }
-    }
-      
-    //delay(10);
+    delay(10);
   }
-
-
 
     //vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
   }
