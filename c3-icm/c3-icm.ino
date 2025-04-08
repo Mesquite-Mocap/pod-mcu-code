804aa99e203416be59eb83e91c723473bbc25c05
Tejaswi Gowda <tejaswi@asu.edu>
2025-04-08 12:48:45 -0700

    acc,gyro,mag

--- c3-icm/c3-icm.ino
+++ c3-icm/c3-icm.ino
@@ -6,9 +6,9 @@
 #include <WiFiClientSecure.h>
 #include <WebSocketsClient.h>
 #include <ESPmDNS.h>
-#include "ICM_20948.h" // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
+#include "ICM_20948.h"  // Click here to get the library: http://librarymanager/All#SparkFun_ICM_20948_IMU
 #define AD0_VAL 0
-ICM_20948_I2C myICM; // Otherwise create an ICM_20948_I2C object
+ICM_20948_I2C myICM;  // Otherwise create an ICM_20948_I2C object
 
 float ax;
 float ay;
@@ -23,7 +23,7 @@ float my;
 float mz;
 
 #include "Button2.h"
-#define BUTTON_PIN  5
+#define BUTTON_PIN 5
 Button2 button;
 
 #include "esp_adc_cal.h"
@@ -33,7 +33,7 @@ int count = 0;
 // ID wifi to connect to
 const char *ssid = "mesquiteMocap";
 const char *password = "movement";
-String serverIP = "0.0.0.0"; // placeholder; mDNS will resolve.
+String serverIP = "0.0.0.0";  // placeholder; mDNS will resolve.
 String dongleName = "mmdongle";
 int sensor_clock = 19;  // updated clock - double check your soldering
 int sensor_data = 18;   // this is from the soldering. double check what you have soldered your data to
@@ -57,7 +57,7 @@ float quatI, quatJ, quatK, quatReal;
 // String bone = "LeftForeArm";
 // String bone = "LeftHand";
 // String bone = "LeftUpLeg";
- String bone = "LeftLeg";
+String bone = "LeftLeg";
 // String bone = "LeftFoot";
 // String bone = "RightArm";
 // String bone = "RightForeArm";
@@ -66,7 +66,7 @@ float quatI, quatJ, quatK, quatReal;
 // String bone = "RightLeg";
 // String bone = "RightFoot";
 // String bone = "Spine";
-// String bone = "Head";  
+// String bone = "Head";
 
 int dccount = 0;
 
@@ -135,22 +135,22 @@ void webSocketEvent(WStype_t type, uint8_t *payload, size_t length) {
       Serial.printf("[WSc] Disconnected!\n");
       dccount++;
       //Serial.println(dccount);
-      if(dccount > 15){
+      if (dccount > 15) {
         esp_deep_sleep_start();
       }
       break;
     case WStype_CONNECTED:  //when connected
       Serial.printf("[WSc] Connected to url: %s\n", payload);
       // send message to server when Connected
-      webSocket.sendTXT("Connected"); 
+      webSocket.sendTXT("Connected");
       dccount = 0;
       webSocket.enableHeartbeat(1000, 100, 100);
       break;
     case WStype_TEXT:  //when you get a message
       Serial.printf("[WSc] get text: %s\n", payload);
       //if(String((char*)payload) == "calibrate"){
-        Serial.println((char*)payload);
-        ESP.restart();
+      Serial.println((char *)payload);
+      ESP.restart();
       //}
       // send message to server
       // webSocket.sendTXT("message here");
@@ -182,37 +182,32 @@ void TaskReadIMU(void *pvParameters);
 #endif
 
 
-void setupIMU()
-{
-    Wire.begin(19,18);
+void setupIMU() {
+  Wire.begin(19, 18);
   delay(500);
   Wire.setClock(400000);
 
   //myICM.enableDebugging();
 
   bool initialized = false;
-  while (!initialized)
-  {
+  while (!initialized) {
 
 
     myICM.begin(Wire, AD0_VAL);
 
     Serial.print(F("Initialization of the sensor returned: "));
     Serial.println(myICM.statusString());
-    if (myICM.status != ICM_20948_Stat_Ok)
-    {
+    if (myICM.status != ICM_20948_Stat_Ok) {
       Serial.println(F("Trying again..."));
       delay(500);
-    }
-    else
-    {
+    } else {
       initialized = true;
     }
   }
 
   Serial.println(F("Device connected."));
 
-  bool success = true; // Use success to show if the DMP configuration was successful
+  bool success = true;  // Use success to show if the DMP configuration was successful
 
   // Initialize the DMP. initializeDMP is a weak function. In this example we overwrite it to change the sample rate (see below)
   success &= (myICM.initializeDMP() == ICM_20948_Stat_Ok);
@@ -231,7 +226,7 @@ void setupIMU()
   //    INV_ICM20948_SENSOR_GEOMAGNETIC_ROTATION_VECTOR (32-bit Geomag RV + heading accuracy)
   //    INV_ICM20948_SENSOR_GEOMAGNETIC_FIELD           (32-bit calibrated compass)
   //    INV_ICM20948_SENSOR_GRAVITY                     (32-bit 6-axis quaternion)
-  //    INV_ICM2094 
+  //    INV_ICM2094
   //    INV_ICM20948_SENSOR_ORIENTATION                 (32-bit 9-axis quaternion + heading accuracy)
 
   // Enable the DMP orientation sensor
@@ -247,9 +242,11 @@ void setupIMU()
   // Setting value can be calculated as follows:
   // Value = (DMP running rate / ODR ) - 1
   // E.g. For a 5Hz ODR rate when DMP is running at 55Hz, value = (55/5) - 1 = 10.
-  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Quat6, 0) == ICM_20948_Stat_Ok); // Set to the maximum
-  //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Accel, 0) == ICM_20948_Stat_Ok); // Set to the maximum
-  //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro, 0) == ICM_20948_Stat_Ok); // Set to the maximum
+  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Quat6, 0) == ICM_20948_Stat_Ok);  // Set to the maximum
+  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Accel, 0) == ICM_20948_Stat_Ok);  // Set to the maximum
+  success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro, 0) == ICM_20948_Stat_Ok);   // Set to the maximum
+
+
   //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Gyro_Calibr, 0) == ICM_20948_Stat_Ok); // Set to the maximum
   //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Cpass, 0) == ICM_20948_Stat_Ok); // Set to the maximum
   //success &= (myICM.setDMPODRrate(DMP_ODR_Reg_Cpass_Calibr, 0) == ICM_20948_Stat_Ok); // Set to the maximum
@@ -267,16 +264,13 @@ void setupIMU()
   success &= (myICM.resetFIFO() == ICM_20948_Stat_Ok);
 
   // Check success
-  if (success)
-  {
+  if (success) {
     Serial.println(F("DMP enabled."));
-  }
-  else
-  {
+  } else {
     Serial.println(F("Enable DMP failed!"));
     Serial.println(F("Please check that you have uncommented line 29 (#define ICM_20948_USE_DMP) in ICM_20948_C.h..."));
     while (1)
-      ; // Do nothing more
+      ;  // Do nothing more
   }
 
 
@@ -311,8 +305,8 @@ void setup() {
   while (WiFiMulti.run() != WL_CONNECTED) {
     delay(100);
     Serial.print(".");
-    if(millis() - start > 1000*20){
-          esp_deep_sleep_start();
+    if (millis() - start > 1000 * 20) {
+      esp_deep_sleep_start();
     }
   }
 
@@ -328,8 +322,8 @@ void setup() {
   while (mdns_init() != ESP_OK) {
     delay(1000);
     Serial.println("Starting MDNS...");
-    if(millis() - st > 1000*20){
-          esp_deep_sleep_start();
+    if (millis() - st > 1000 * 20) {
+      esp_deep_sleep_start();
     }
   }
 
@@ -343,13 +337,13 @@ void setup() {
     Serial.println("Resolving host...");
     delay(250);
     serverIp = MDNS.queryHost(dongleName);
-    if(millis() - st2 > 1000*20){
-          esp_deep_sleep_start();
+    if (millis() - st2 > 1000 * 20) {
+      esp_deep_sleep_start();
     }
   }
-  
 
-  	
+
+
   Serial.println(serverIp.toString());
 
   serverIP = serverIp.toString();
@@ -404,13 +398,12 @@ void TaskWifi(void *pvParameters) {
 
     if (millis() > (prev_ms + (1000 / fps))) {
       fcount++;
-      if(fcount%2 == 0){
+      if (fcount % 2 == 0) {
         digitalWrite(3, HIGH);
+      } else {
+        digitalWrite(3, LOW);
       }
-      else{
-         digitalWrite(3, LOW);
-      }
-      String url = "{\"id\":\"" + mac_address + "\", \"count\":\"" + String(fcount) +"\", \"millis\":\"" + String(millis()) +"\", \"bone\":\"" + bone + "\", \"x\":" + quat.x + ", \"y\":" + quat.y + ", \"z\":" + quat.z + ", \"w\":" + quat.w + ", \"batt\":" + (batt_v / 4192) + ",\"ax\":" + ax + ", \"ay\":" + ay + ", \"az\":" + az + ", \"gx\":" + gx + ", \"gy\":" + gy + ", \"gz\":" + gz + ", \"mx\":" + mx + ", \"my\":" + my + ", \"mz\":" + mz + "}";
+      String url = "{\"id\":\"" + mac_address + "\", \"count\":\"" + String(fcount) + "\", \"millis\":\"" + String(millis()) + "\", \"bone\":\"" + bone + "\", \"x\":" + quat.x + ", \"y\":" + quat.y + ", \"z\":" + quat.z + ", \"w\":" + quat.w + ", \"batt\":" + (batt_v / 4192) + ",\"ax\":" + ax + ", \"ay\":" + ay + ", \"az\":" + az + ", \"gx\":" + gx + ", \"gy\":" + gy + ", \"gz\":" + gz + ", \"mx\":" + mx + ", \"my\":" + my + ", \"mz\":" + mz + "}";
       Serial.println(url);
 
       webSocket.sendTXT(url.c_str());
@@ -423,7 +416,7 @@ void TaskWifi(void *pvParameters) {
           ESP.restart();
         }
         //checkLocking();
-       }
+      }
     }
     // vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
   }
@@ -442,55 +435,55 @@ void TaskReadIMU(void *pvParameters) {
     }
 
 
-icm_20948_DMP_data_t data;
-  myICM.readDMPdataFromFIFO(&data);
+    icm_20948_DMP_data_t data;
+    myICM.readDMPdataFromFIFO(&data);
 
-  if ((myICM.status == ICM_20948_Stat_Ok) || (myICM.status == ICM_20948_Stat_FIFOMoreDataAvail)) // Was valid data available?
-  {
-    //Serial.print(F("Received data! Header: 0x")); // Print the header in HEX so we can see what data is arriving in the FIFO
-    //if ( data.header < 0x1000) Serial.print( "0" ); // Pad the zeros
-    //if ( data.header < 0x100) Serial.print( "0" );
-    //if ( data.header < 0x10) Serial.print( "0" );
-    //Serial.println( data.header, HEX );
-
-    if ((data.header & DMP_header_bitmap_Quat6) > 0) // We have asked for GRV data so we should receive Quat6
+    if ((myICM.status == ICM_20948_Stat_Ok) || (myICM.status == ICM_20948_Stat_FIFOMoreDataAvail))  // Was valid data available?
     {
-      // Q0 value is computed from this equation: Q0^2 + Q1^2 + Q2^2 + Q3^2 = 1.
-      // In case of drift, the sum will not add to 1, therefore, quaternion data need to be corrected with right bias values.
-      // The quaternion data is scaled by 2^30.
+      //Serial.print(F("Received data! Header: 0x")); // Print the header in HEX so we can see what data is arriving in the FIFO
+      //if ( data.header < 0x1000) Serial.print( "0" ); // Pad the zeros
+      //if ( data.header < 0x100) Serial.print( "0" );
+      //if ( data.header < 0x10) Serial.print( "0" );
+      //Serial.println( data.header, HEX );
 
-      //Serial.printf("Quat6 data is: Q1:%ld Q2:%ld Q3:%ld\r\n", data.Quat6.Data.Q1, data.Quat6.Data.Q2, data.Quat6.Data.Q3);
+      if ((data.header & DMP_header_bitmap_Quat6) > 0)  // We have asked for GRV data so we should receive Quat6
+      {
+        // Q0 value is computed from this equation: Q0^2 + Q1^2 + Q2^2 + Q3^2 = 1.
+        // In case of drift, the sum will not add to 1, therefore, quaternion data need to be corrected with right bias values.
+        // The quaternion data is scaled by 2^30.
 
-      // Scale to +/- 1
-      double q1 = ((double)data.Quat6.Data.Q1) / 1073741824.0; // Convert to double. Divide by 2^30
-      double q2 = ((double)data.Quat6.Data.Q2) / 1073741824.0; // Convert to double. Divide by 2^30
-      double q3 = ((double)data.Quat6.Data.Q3) / 1073741824.0; // Convert to double. Divide by 2^30
+        //Serial.printf("Quat6 data is: Q1:%ld Q2:%ld Q3:%ld\r\n", data.Quat6.Data.Q1, data.Quat6.Data.Q2, data.Quat6.Data.Q3);
+
+        // Scale to +/- 1
+        double q1 = ((double)data.Quat6.Data.Q1) / 1073741824.0;  // Convert to double. Divide by 2^30
+        double q2 = ((double)data.Quat6.Data.Q2) / 1073741824.0;  // Convert to double. Divide by 2^30
+        double q3 = ((double)data.Quat6.Data.Q3) / 1073741824.0;  // Convert to double. Divide by 2^30
 
 
-      // Convert the quaternions to Euler angles (roll, pitch, yaw)
-      // https://en.wikipedia.org/w/index.php?title=Conversion_between_quaternions_and_Euler_angles&section=8#Source_code_2
+        // Convert the quaternions to Euler angles (roll, pitch, yaw)
+        // https://en.wikipedia.org/w/index.php?title=Conversion_between_quaternions_and_Euler_angles&section=8#Source_code_2
 
-      double q0 = sqrt(1.0 - ((q1 * q1) + (q2 * q2) + (q3 * q3)));
+        double q0 = sqrt(1.0 - ((q1 * q1) + (q2 * q2) + (q3 * q3)));
 
-      double q2sqr = q2 * q2;
+        double q2sqr = q2 * q2;
 
-      // roll (x-axis rotation)
-      double t0 = +2.0 * (q0 * q1 + q2 * q3);
-      double t1 = +1.0 - 2.0 * (q1 * q1 + q2sqr);
-      double roll = atan2(t0, t1) * 180.0 / PI;
+        // roll (x-axis rotation)
+        double t0 = +2.0 * (q0 * q1 + q2 * q3);
+        double t1 = +1.0 - 2.0 * (q1 * q1 + q2sqr);
+        double roll = atan2(t0, t1) * 180.0 / PI;
 
-      // pitch (y-axis rotation)
-      double t2 = +2.0 * (q0 * q2 - q3 * q1);
-      t2 = t2 > 1.0 ? 1.0 : t2;
-      t2 = t2 < -1.0 ? -1.0 : t2;
-      double pitch = asin(t2) * 180.0 / PI;
+        // pitch (y-axis rotation)
+        double t2 = +2.0 * (q0 * q2 - q3 * q1);
+        t2 = t2 > 1.0 ? 1.0 : t2;
+        t2 = t2 < -1.0 ? -1.0 : t2;
+        double pitch = asin(t2) * 180.0 / PI;
 
-      // yaw (z-axis rotation)
-      double t3 = +2.0 * (q0 * q3 + q1 * q2);
-      double t4 = +1.0 - 2.0 * (q2sqr + q3 * q3);
-      double yaw = atan2(t3, t4) * 180.0 / PI;
+        // yaw (z-axis rotation)
+        double t3 = +2.0 * (q0 * q3 + q1 * q2);
+        double t4 = +1.0 - 2.0 * (q2sqr + q3 * q3);
+        double yaw = atan2(t3, t4) * 180.0 / PI;
 
-/*
+        /*
       Serial.print(q0, 3);
       Serial.print(" ");
       Serial.print(q1, 3);
@@ -501,49 +494,57 @@ icm_20948_DMP_data_t data;
       Serial.println();
       */
 
-      quat.w = q0;
-      quat.x = q1;
-      quat.y = q2;
-      quat.z = q3;
+        quat.w = q0;
+        quat.x = q1;
+        quat.y = q2;
+        quat.z = q3;
 
 
-      // read accelerometer data
-      ax = myICM.accX();
-      ay = myICM.accY();
-      az = myICM.accZ();
+        if ((data.header & DMP_header_bitmap_Accel) > 0)  // Check the packet contains Accel data
+        {
+          ax = (float)data.Raw_Accel.Data.X;  // Extract the raw accelerometer data
+          ay = (float)data.Raw_Accel.Data.Y;
+          az = (float)data.Raw_Accel.Data.Z;
+        }
 
-      // read gyro data
-      ax = myICM.gyrX();
-      ay = myICM.gyrY();
-      az = myICM.gyrZ();
+       if ( (data.header & DMP_header_bitmap_Gyro) > 0 ) // Check for Gyro
+       {
+         gx = (float)data.Raw_Gyro.Data.X; // Extract the raw gyro data
+         gy = (float)data.Raw_Gyro.Data.Y;
+         gz = (float)data.Raw_Gyro.Data.Z;
 
-      // read mag data
-      ax = myICM.magX();
-      ay = myICM.magY();
-      az = myICM.magZ();
+       }
+    
+       if ( (data.header & DMP_header_bitmap_Compass) > 0 ) // Check for Compass
+       {
+         mx = (float)data.Compass.Data.X; // Extract the compass data
+         my = (float)data.Compass.Data.Y;
+         mz = (float)data.Compass.Data.Z;
+       }
 
+
+
+      }
     }
 
-  }
-
-  if (myICM.status != ICM_20948_Stat_FIFOMoreDataAvail) // If more data is available then we should read it right away - and not delay
-  {
-    delay(10);
-  }
+    if (myICM.status != ICM_20948_Stat_FIFOMoreDataAvail)  // If more data is available then we should read it right away - and not delay
+    {
+      delay(10);
+    }
 
     //vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
   }
 }
 
-void longClickDetected(Button2& btn) {
-    Serial.print("---------------- long click #");
-    Serial.print(btn.getLongClickCount());
-    Serial.println(" detected");
-      digitalWrite(3, LOW);
-      webSocket.sendTXT("disconnected"); 
-      webSocket.disconnect();
+void longClickDetected(Button2 &btn) {
+  Serial.print("---------------- long click #");
+  Serial.print(btn.getLongClickCount());
+  Serial.println(" detected");
+  digitalWrite(3, LOW);
+  webSocket.sendTXT("disconnected");
+  webSocket.disconnect();
 }
 
-void longClick(Button2& btn) {
-    esp_deep_sleep_start();
+void longClick(Button2 &btn) {
+  esp_deep_sleep_start();
 }
